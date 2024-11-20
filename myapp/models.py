acec73a81186dd91bdbfbb3e56ec6b8197e95981
Talha zulfiqar <csharpjava0@gmail.com>
2024-11-20 15:07:55 +0000

    add users model and migration for user management

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
+
+# Model for Users
+class users(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+# Model For Products
 class products(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
